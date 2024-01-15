144a0942fb3afb13c2ab63b3068e78aa6d6d1a29
Paulino Auyon <pauyon@jgboswell.com>
2024-01-15 14:30:41 -0800

    Add component that checks internet connection

---
+++ BlazorServerApp.Client/Components/Connection.razor.cs
@@ -0,0 +1,54 @@
+﻿using BlazorServerApp.Client.Shared.Dialogs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace BlazorServerApp.Client.Components
+{
+    /// <summary>
+    /// Component that displays whether there is internet connection
+    /// </summary>
+    public partial class Connection
+    {
+        [Inject]
+        public IJSRuntime? JsRuntime { get; set; }
+
+        [Inject]
+        public IDialogService? DialogService { get; set; }
+
+        private void OpenDialog()
+        {
+            var parameters = new DialogParameters<ConnectionDialog>
+            {
+                { x => x.IsOnline, IsOnline }
+            };
+
+            var options = new DialogOptions { CloseOnEscapeKey = true, MaxWidth = MaxWidth.Large };
+            DialogService!.Show<ConnectionDialog>("Connection Info", parameters, options);
+        }
+
+        public bool IsOnline { get; set; }
+
+        [JSInvokable("Connection.StatusChanged")]
+        public void OnConnectionStatusChanged(bool isOnline)
+        {
+            if (IsOnline != isOnline)
+            {
+                IsOnline = isOnline;
+            }
+
+            StateHasChanged();
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            JsRuntime!.InvokeVoidAsync("Connection.Initialize", DotNetObjectReference.Create(this));
+        }
+
+        public void Dispose()
+        {
+            JsRuntime!.InvokeVoidAsync("Connection.Dispose");
+        }
+    }
+}
