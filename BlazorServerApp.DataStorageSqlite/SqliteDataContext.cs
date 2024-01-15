5c6fc010a362a687e8a051a0d68e16064d7b8786
Paulino Auyon <pauyon@jgboswell.com>
2024-01-15 15:26:43 -0800

    Add Sqlite and a user model

---
+++ BlazorServerApp.DataStorageSqlite/SqliteDataContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorServerApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerApp.DataStorageSqlite
+{
+    public class SqliteDataContext : DbContext
+    {
+        public SqliteDataContext(DbContextOptions<SqliteDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
